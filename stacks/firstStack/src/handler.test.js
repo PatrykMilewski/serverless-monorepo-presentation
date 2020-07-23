097023e03798b6d70c69619e27ccecd1bd35ab3d
Patryk Milewski <patryk.milewski@merapar.com>
2020-07-23 22:59:04 +0200

    enchanced yarn and lerna monorepo with more custom scripts in root package.json

---
+++ stacks/firstStack/src/handler.test.js
@@ -0,0 +1,3 @@
+it('firstStack example test', () => {
+    expect(1).toBeGreaterThanOrEqual(1)
+})
---
+++ stacks/secondStack/src/handler.test.js
@@ -0,0 +1,3 @@
+it('secondStack example test', () => {
+    expect(1).toBeGreaterThanOrEqual(2)
+})
